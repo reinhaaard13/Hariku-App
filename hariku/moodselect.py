41ac943cc33c6196e3e8aa389965a416b453acb3
reinhaaard13 <reinhardkevin.rk@gmail.com>
2021-06-13 02:22:24 +0700

    delete calculate mood

--- hariku/moodselect.py
+++ hariku/moodselect.py
@@ -7,11 +7,18 @@ class Mood:
     def count_mood(self, words_in_diary):
         list_of_words = self.split_text(words_in_diary)
         try:
-            self.score = self.calculate_mood(list_of_words)
+            score = 0
+            with open("hariku/keywords.json",) as json_keywords:
+                keywords = json.load(json_keywords)
+                for word in list_of_words:
+                    if word in keywords['positive']:
+                        score += 1
+                    elif word in keywords['negative']:
+                        score -= 1
+                self.score = score
             return self.score
         except IndexError:
             pass
-        # print(self.score)
 
     def split_text(self, words_in_diary):
         punctuations = '''.?!-~,"':/()<>'''
@@ -22,15 +29,4 @@ class Mood:
                 no_punctuations += char
         list_of_words = no_punctuations.split()
         return list_of_words
-
-    def calculate_mood(self, list_of_words):
-        score = 0
-        with open("hariku/keywords.json",) as json_keywords:
-            keywords = json.load(json_keywords)
-            for word in list_of_words:
-                if word in keywords['positive']:
-                    score += 1
-                elif word in keywords['negative']:
-                    score -= 1
-        return score
 
